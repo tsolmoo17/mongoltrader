c08cd10d4681a2995f7d585e1840b712e019da05
Tsolmon <39569843+tsolmoo17@users.noreply.github.com>
2019-05-27 21:35:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var target_date = new Date().getTime() + (1000*3600*48); // set the countdown date
+var target_date = new Date().getTime() + (10000*3600*48); // set the countdown date
 var days, hours, minutes, seconds; // variables for time units
 
 var countdown = document.getElementById("tiles"); // get tag element
